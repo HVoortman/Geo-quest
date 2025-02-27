3a503a3cab0cf2c09875d7ef9bc702034031d548
HVoortman <h.voortman2002@gmail.com>
2025-02-27 19:54:22 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+const locations = [
+    { lat: 55.9533, lon: -3.1883, question: "What is the capital of Scotland?", answer: "Edinburgh" },
+    { lat: 57.4778, lon: -4.2247, question: "What famous monster is said to live nearby?", answer: "Nessie" }
+];
